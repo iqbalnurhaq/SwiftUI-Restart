35f9bc975feba0cd9f20f8d7e5c65564e389760d
Iqbal Nur Haq <iqbalnur833@gmail.com>
2022-10-20 22:13:15 +0700

    Reusable Component Circle

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -15,10 +15,13 @@ struct HomeView: View {
             
             // MARK: -HEADER
             Spacer()
-            Image("character-2")
-                .resizable()
-                .scaledToFit()
+            ZStack {
+                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
                 .padding()
+            }
             
             //MARK: -CENTER
             Text("The time that leads to mastery is dependent on the intensity of our focus.")
