db2d529d1bdb23f60e674799e29667f4dbd296ec
Iqbal Nur Haq <iqbalnur833@gmail.com>
2022-10-22 14:14:14 +0700

    Add Animation

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct HomeView: View {
     
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    @State private var isAnimating: Bool = false
+    
+    
     var body: some View {
         VStack(spacing: 20) {
             
@@ -20,7 +23,13 @@ struct HomeView: View {
                 Image("character-2")
                     .resizable()
                     .scaledToFit()
-                .padding()
+                    .padding()
+                    .offset(y: isAnimating ? 35 : -35)
+                    .animation(
+                        Animation.easeOut(duration: 4)
+                            .repeatForever()
+                        , value: isAnimating
+                    )
             }
             
             //MARK: -CENTER
@@ -35,7 +44,9 @@ struct HomeView: View {
             Spacer()
             
             Button(action: {
-                isOnboardingViewActive = true
+                withAnimation {
+                    isOnboardingViewActive = true
+                }
             }){
                 Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
                     .imageScale(.large)
@@ -48,6 +59,11 @@ struct HomeView: View {
             .buttonBorderShape(.capsule)
             .controlSize(.large)
         }
+        .onAppear(perform:  {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                isAnimating = true
+            })
+        })
     }
 }
 
